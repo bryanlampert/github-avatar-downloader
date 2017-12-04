bbdb4a7255356812f8c2e75aadf3ef9e6618828c
bryanlampert <bryanlampert@gmail.com>
2017-12-04 21:32:03 +0000

    Completed step 6

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,26 @@
 var request = require('request');
+var authToken = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + authToken.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
